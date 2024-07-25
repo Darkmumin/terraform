f7a2785189b9cd1fb58d5868625ff2b5a45636d8
Darkmumin <Darkmumin@users.noreply.github.com>
2024-07-25 17:32:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
+  Hola = Hola.var
 }
 resource "aws_instance" "nginx-server" {
   ami           = "ami-0583d8c7a9c35822c"
