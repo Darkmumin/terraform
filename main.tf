1e902ed4c22187c55955b1a195b84d820928fe68
Darkmumin <edi.rodriguez.saza@wom.co>
2024-07-25 17:08:58 -0500

    primer commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+resource "aws_instance" "nginx-server" {
+  ami           = "ami-0583d8c7a9c35822c"
+  instance_type = "t2.nano"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "access_key" {
+  type        = string
+  description = "Access Key for access to AWS"
+  sensitive   = true
+}
+
+variable "secret_key" {
+  type        = string
+  description = "Secret Key for access to AWS"
+  sensitive   = true
+}
+
