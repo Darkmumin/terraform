ef798ce7959c4eece61ced90012d24bd0e19e4ed
Darkmumin <edi.rodriguez.saza@wom.co>
2024-07-25 17:35:31 -0500

    prueba

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ provider "aws" {
   region     = "us-east-1"
   access_key = var.access_key
   secret_key = var.secret_key
-  Hola = Hola.var
 }
 resource "aws_instance" "nginx-server" {
   ami           = "ami-0583d8c7a9c35822c"
